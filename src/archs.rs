a87afaaf7487d434fbb228af73ab46800ab36751
Peter Jin <peterhaijin@gmail.com>
2016-10-18 10:55:34 -0700

    Implement ResNet-18 for ImageNet.

--- src/archs.rs
+++ src/archs.rs
@@ -257,3 +257,139 @@ pub fn build_cifar10_resnet56_loss(batch_sz: usize, stream: DeviceStream) -> Rc<
   let loss = DeviceSoftmaxNLLClassLoss::new(loss_cfg, OpCapability::Backward, affine, 0, stream.clone());
   loss
 }
+
+pub fn build_imagenet_resnet18_loss(batch_sz: usize, stream: DeviceStream) -> Rc<RefCell<DeviceSoftmaxNLLClassLoss<SampleItem>>> {
+  let input_cfg = VarInputOperatorConfig{
+    batch_sz:   batch_sz,
+    max_stride: 16 * 480 * 480 * 3,
+    out_dim:    (224, 224, 3),
+    preprocs:   vec![
+      VarInputPreproc::RandomResize2d{hi: 480, lo: 256, phases: vec![OpPhase::Learning]},
+      VarInputPreproc::RandomResize2d{hi: 256, lo: 256, phases: vec![OpPhase::Inference]},
+      VarInputPreproc::RandomCrop2d{crop_w: 224, crop_h: 224, pad_w: 0, pad_h: 0, phases: vec![OpPhase::Learning]},
+      VarInputPreproc::CenterCrop2d{crop_w: 224, crop_h: 224, phases: vec![OpPhase::Inference]},
+      VarInputPreproc::RandomFlipX{phases: vec![OpPhase::Learning]},
+      //VarInputPreproc::ChannelShift{shift: vec![125.0, 123.0, 114.0]},
+      VarInputPreproc::Scale{scale: 1.0 / 255.0},
+      //VarInputPreproc::AddPixelwisePCALigtingNoise{},
+    ],
+  };
+  let conv1_cfg = BatchNormConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (224, 224, 3),
+    kernel_w:   7,  kernel_h:   7,
+    stride_w:   2,  stride_h:   2,
+    pad_w:      3,  pad_h:      3,
+    out_chan:   64,
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let pool1_cfg = Pool2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (112, 112, 64),
+    pool_w:     3,  pool_h:     3,
+    stride_w:   2,  stride_h:   2,
+    pad_w:      1,  pad_h:      1,
+    kind:       PoolKind::Max,
+  };
+  let res1_cfg = ResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (56, 56, 64),
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let proj_res2_cfg = ProjResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (56, 56, 64),
+    stride_w:   2,  stride_h:   2,
+    out_chan:   128,
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let res2_cfg = ResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (28, 28, 128),
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let proj_res3_cfg = ProjResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (28, 28, 128),
+    stride_w:   2,  stride_h:   2,
+    out_chan:   256,
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let res3_cfg = ResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (14, 14, 256),
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let proj_res4_cfg = ProjResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (14, 14, 256),
+    stride_w:   2,  stride_h:   2,
+    out_chan:   512,
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let res3_cfg = ResidualConv2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (7, 7, 512),
+    avg_rate:   RESNET_AVG_RATE,
+    epsilon:    RESNET_EPSILON,
+    act_kind:   ActivationKind::Rect,
+    w_init:     ParamInitKind::Kaiming,
+  };
+  let pool_cfg = Pool2dOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     (7, 7, 512),
+    pool_w:     7,  pool_h:     7,
+    stride_w:   7,  stride_h:   7,
+    pad_w:      0,
+    pad_h:      0,
+    kind:       PoolKind::Average,
+  };
+  let affine_cfg = AffineOperatorConfig{
+    batch_sz:   batch_sz,
+    in_dim:     512,
+    out_dim:    1000,
+    act_kind:   ActivationKind::Identity,
+    w_init:     ParamInitKind::Xavier,
+    //w_init:     ParamInitKind::Kaiming,
+  };
+  let loss_cfg = ClassLossConfig{
+    batch_sz:       batch_sz,
+    num_classes:    10,
+  };
+  let input = DeviceVarInputOperator::new(input_cfg, OpCapability::Backward, stream.clone());
+  let conv1 = DeviceBatchNormConv2dOperator::new(conv1_cfg, OpCapability::Backward, input, 0, stream.clone());
+  let pool1 = DevicePool2dOperator::new(pool1_cfg, OpCapability::Backward, conv1, 0, stream.clone());
+  let res1_1 = DeviceResidualConv2dOperator::new(res1_cfg, OpCapability::Backward, pool1, 0, stream.clone());
+  let res1_2 = DeviceResidualConv2dOperator::new(res1_cfg, OpCapability::Backward, res1_1, 0, stream.clone());
+  let res2_1 = DeviceProjResidualConv2dOperator::new(proj_res2_cfg, OpCapability::Backward, res1_2, 0, stream.clone());
+  let res2_2 = DeviceResidualConv2dOperator::new(res2_cfg, OpCapability::Backward, res2_1, 0, stream.clone());
+  let res3_1 = DeviceProjResidualConv2dOperator::new(proj_res3_cfg, OpCapability::Backward, res2_2, 0, stream.clone());
+  let res3_2 = DeviceResidualConv2dOperator::new(res3_cfg, OpCapability::Backward, res3_1, 0, stream.clone());
+  let res4_1 = DeviceProjResidualConv2dOperator::new(proj_res3_cfg, OpCapability::Backward, res3_2, 0, stream.clone());
+  let res4_2 = DeviceResidualConv2dOperator::new(res3_cfg, OpCapability::Backward, res4_1, 0, stream.clone());
+  let pool = DevicePool2dOperator::new(pool_cfg, OpCapability::Backward, res4_2, 0, stream.clone());
+  let affine = DeviceAffineOperator::new(affine_cfg, OpCapability::Backward, pool, 0, stream.clone());
+  let loss = DeviceSoftmaxNLLClassLoss::new(loss_cfg, OpCapability::Backward, affine, 0, stream.clone());
+  loss
+}
